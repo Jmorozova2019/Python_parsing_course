69bed9ac5bcf25e1b4d15abbee854dd537804c41
Jmorozova2019 <51861665+Jmorozova2019@users.noreply.github.com>
2024-04-15 22:48:09 +0300

    Create 5_6_3_task.py
    # Задача на степике https://stepik.org/lesson/1108387/step/6?unit=1119656

---
+++ 5_6_3_task.py
@@ -0,0 +1,28 @@
+# Задача на степике https://stepik.org/lesson/1108387/step/6?unit=1119656
+
+import time
+from pprint import pprint
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+base_url = 'https://parsinger.ru/methods/5/'
+
+with webdriver.Chrome() as webdriver:
+    webdriver.get(base_url + 'index.html')
+    link_tags = webdriver.find_elements(By.TAG_NAME, 'a')
+    list_url = [link_tag.get_attribute('href') for link_tag in webdriver.find_elements(By.TAG_NAME, 'a')]
+
+    list_expiry = {}
+    for url in list_url:
+        webdriver.get(url)
+        cookies = webdriver.get_cookies()
+        pprint(cookies)
+        for cookie in cookies:
+            list_expiry[url] = cookie['expiry']
+        webdriver.quit()
+
+ssd = sorted(list_expiry, key=list_expiry.__getitem__)
+k = ssd[-1]
+webdriver.get(k)
+print(webdriver.find_elements(By.ID, 'result').text)
